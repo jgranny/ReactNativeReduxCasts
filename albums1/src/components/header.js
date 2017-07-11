15f07ffc43ab4aef19342d7d3412fb4540642f8f
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-07-11 16:45:35 -0500

    Init

---
+++ albums1/src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => (
+  <Text>Albums</Text>
+);
+
+// Make component available to other parts of the app
+export default Header;
