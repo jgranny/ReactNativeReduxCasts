50233173e397b57999579278c0b45fa7ec17d52c
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-07-13 14:01:26 -0500

    Authentication

--- manager1/src/App.js
+++ manager1/src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import firebaseConfig from '../firebaseConfig';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
-          <Text>Hello</Text>
+          <LoginForm>Hello</LoginForm>
         </View>
       </Provider>
     );
