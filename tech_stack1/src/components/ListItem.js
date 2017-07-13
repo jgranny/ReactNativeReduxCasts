9005a33dbaea7e0475bb033efa90338c0412f02c
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-07-13 12:04:39 -0500

    Display library item descriptions

--- tech_stack1/src/components/ListItem.js
+++ tech_stack1/src/components/ListItem.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+// * as actions means import everything from the actions file, and give me
+// access to it on the actions variable
+import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
+
 
   render() {
     const { titleStyle } = styles;
-    
+    const { id, title } = this.props.library;
+
     return (
-      <CardSection>
-        <Text style={titleStyle}>{this.props.library.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
+
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded };
+};
 
 const styles = {
   titleStyle: {
@@ -22,4 +52,7 @@ const styles = {
   }
 };
 
-export default ListItem;
+//First argument is exclusively for mapStateToProps, so if you want to exclude
+// it, you must pass null in the first argument.
+// Here, we are passing all the actions to ListItem as props
+export default connect(mapStateToProps, actions)(ListItem);
