767de0f4a82b4beb21c5cdc9e2bb73b3329787d3
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-07-13 14:41:25 -0500

    Password auth

--- manager1/src/reducers/AuthReducer.js
+++ manager1/src/reducers/AuthReducer.js
@@ -1,8 +1,22 @@
-const INITIAL_STATE = { email: ''};
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  email: '',
+  password: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMAIL_CHANGED:
+      //Create a new object with what is currently in state, and then throw
+      // the new email into the new object
+      return { ...state, email: action.payload };
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload };
     default:
       return state;
   }
-}
+};
